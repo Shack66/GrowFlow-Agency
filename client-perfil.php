92527e009b993806b28c9c4635b43d3ab501f920
paolaran <165974832+paolaran@users.noreply.github.com>
2025-12-03 21:56:46 -0500

    .

--- client-perfil.php
+++ client-perfil.php
@@ -1,13 +1,4 @@
-<?php
-require_once 'auth.php';
-require_once 'db.php';
 
-// Verificar que sea cliente
-if (!esCliente()) {
-    header("Location: login.php");
-    exit();
-}
-?>
 
 <!DOCTYPE html>
 <html lang="es">
